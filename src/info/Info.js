260ff241dd5c79c705f467d447f164de975101f2
Anjum00 <105652255+Anjum00@users.noreply.github.com>
2025-03-11 19:55:27 +0530

    Update Info.js

--- src/info/Info.js
+++ src/info/Info.js
@@ -5,36 +5,19 @@ import mock6 from "../img/mock6.png"
 import mock4 from "../img/mock4.png"
 import mock5 from "../img/mock5.png"
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
-
 export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
-
-/* 
-This variable will change the layout of the website from multipage to single, scrollable page
-*/
 export let singlePage = false;
 
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
 
 export const info = {
     firstName: "Anjum",
     lastName: "Saiyad",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Full Stack Developer",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
+    initials: "Anjum",
+    position: "a React Native Developer",
+    selfPortrait: self,
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    miniBio: [
         {
             emoji: '☕',
             text: 'fueled by coffee'
@@ -45,7 +28,7 @@ export const info = {
         },
         {
             emoji: "💼",
-            text: "Full stack devloper intern at Elsner Technology"
+            text: "React Native devloper"
         },
         {
             emoji: "📧",
@@ -82,15 +65,12 @@ export const info = {
             icon: "fa fa-twitter",
             label: 'twitter'
         }
-        // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-        // Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "I am Anjum, a Full-Stack Developer based in Ahmedabad. I completed my BCA from MKBU University in 2023 and have since gained practical experience through a full-stack developer internship at Elsner Technology Ahmedabad. My expertise includes HTML, CSS, JavaScript, and React, where I specialize in building dynamic, responsive web applications. During my internship, I developed skills in both frontend and backend development, integrating user interfaces with backend services to create seamless and efficient solutions. I am passionate about leveraging my technical skills to solve complex problems and deliver high-quality software.",
+    bio: "I am Anjum, a React Native Developer at Silicon Valley Infomedia, based in Ahmedabad. I previously gained practical experience through a full-stack developer internship at Elsner Technology Ahmedabad. My expertise includes React Native, JavaScript, HTML, CSS, and React, where I specialize in building dynamic, responsive, and cross-platform mobile applications. My experience spans both frontend and backend development, integrating user interfaces with backend services to create seamless and efficient solutions. I am passionate about leveraging my technical skills to solve complex problems and deliver high-quality software experiences.Let me know if you need further adjustments! 🚀",
     skills:
     {
-        proficientWith: ['javascript', 'react', 'git', 'github', 'html', 'css', 'figma'],
-        exposedTo: ['nodejs', 'adobe illustrator']
+        proficientWith: ['react native','javascript', 'react', 'github','html', 'css', 'figma',],
     }
     ,
     hobbies: [
@@ -110,14 +90,12 @@ export const info = {
             label: 'cooking',
             emoji: '🌶'
         }
-        // Same as above, change the emojis to match / relate to your hobbies or interests.
-        // You can also remove the emojis if you'd like, I just think they look cute :P
     ],
-    portfolio: [ // This is where your portfolio projects will be detailed
+    portfolio: [ 
         {
             title: "HTML/CSS Project",
-            live: "https://anjum00.github.io/HTMlFigmaProject/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/Anjum00/HTMlFigmaProject", // this should be a link to the **repository** of the project, where the code is hosted.
+            live: "https://anjum00.github.io/HTMlFigmaProject/", 
+            source: "https://github.com/Anjum00/HTMlFigmaProject",
             image: mock1
         },
         {
